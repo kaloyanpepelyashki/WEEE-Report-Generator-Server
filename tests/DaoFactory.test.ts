759281e70b8b99e6cc0c7039ff71176859b9a8a8
Kaloyan Pepelyashki <93147724+kaloyanpepelyashki@users.noreply.github.com>
2024-10-25 10:14:36 +0200

    Update DaoFactory.test.ts

--- tests/DaoFactory.test.ts
+++ tests/DaoFactory.test.ts
@@ -1,40 +1,40 @@
-// import DaoFactory from "../Factory/DaoFactory";
-// import ProductsDAO from "../DAOs/ProductsDAO";
-// import OrdersDAO from "../DAOs/OrdersDAO";
-// import CollectionsDAO from "../DAOs/CollectionsDAO";
-// import CollectionsGraphDAO from "../DAOs/CollectionsGraphDAO";
+import DaoFactory from "../Factory/DaoFactory";
+import ProductsDAO from "../DAOs/ProductsDAO";
+import OrdersDAO from "../DAOs/OrdersDAO";
+import CollectionsDAO from "../DAOs/CollectionsDAO";
+import CollectionsGraphDAO from "../DAOs/CollectionsGraphDAO";
 
-// jest.mock("../DAOs/ProductsDAO");
-// jest.mock("../DAOs/OrdersDAO");
-// jest.mock("../DAOs/CollectionsDAO");
-// jest.mock("../DAOs/CollectionsGraphDAO");
+jest.mock("../DAOs/ProductsDAO");
+jest.mock("../DAOs/OrdersDAO");
+jest.mock("../DAOs/CollectionsDAO");
+jest.mock("../DAOs/CollectionsGraphDAO");
 
-// describe("DaoFactory tests", () => {
-//   it("should return an instance of ProductsDAO when requested", () => {
-//     const factory = new DaoFactory("test-token", "test-host");
-//     const dao = factory.getDAO<ProductsDAO>("productsDao");
+describe("DaoFactory tests", () => {
+  it("should return an instance of ProductsDAO when requested", () => {
+    const factory = new DaoFactory("test-token", "test-host");
+    const dao = factory.getDAO<ProductsDAO>("productsDao");
 
-//     expect(dao).toBeInstanceOf(ProductsDAO);
-//   });
+    expect(dao).toBeInstanceOf(ProductsDAO);
+  });
 
-//   it("should return an instance of CollectionsDAO when requested", () => {
-//     const factory = new DaoFactory("test-token", "test-host");
-//     const dao = factory.getDAO<CollectionsDAO>("collectionsRestDao");
+  it("should return an instance of CollectionsDAO when requested", () => {
+    const factory = new DaoFactory("test-token", "test-host");
+    const dao = factory.getDAO<CollectionsDAO>("collectionsRestDao");
 
-//     expect(dao).toBeInstanceOf(CollectionsDAO);
-//   });
+    expect(dao).toBeInstanceOf(CollectionsDAO);
+  });
 
-//   it("should return an instance of OrdersDAO when requested", () => {
-//     const factory = new DaoFactory("test-token", "test-host");
-//     const dao = factory.getDAO<OrdersDAO>("ordersDao");
+  it("should return an instance of OrdersDAO when requested", () => {
+    const factory = new DaoFactory("test-token", "test-host");
+    const dao = factory.getDAO<OrdersDAO>("ordersDao");
 
-//     expect(dao).toBeInstanceOf(OrdersDAO);
-//   });
+    expect(dao).toBeInstanceOf(OrdersDAO);
+  });
 
-//   it("should return an instance of CollectionsGraphDAO when requested", () => {
-//     const factory = new DaoFactory("test-token", "test-host");
-//     const dao = factory.getDAO<CollectionsGraphDAO>("collectionsGraphDao");
+  it("should return an instance of CollectionsGraphDAO when requested", () => {
+    const factory = new DaoFactory("test-token", "test-host");
+    const dao = factory.getDAO<CollectionsGraphDAO>("collectionsGraphDao");
 
-//     expect(dao).toBeInstanceOf(CollectionsGraphDAO);
-//   });
-// });
+    expect(dao).toBeInstanceOf(CollectionsGraphDAO);
+  });
+});
